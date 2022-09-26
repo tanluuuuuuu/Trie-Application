8d46a6154a727bb6223122b84cb7727cfeb9bdb6
tanluuuuuuuuuu <luungo@techbee.dev>
2022-09-26 10:08:38 +0700

    Add Trie

--- pages/citizen_info/index.js
+++ pages/citizen_info/index.js
@@ -1,23 +1,64 @@
-import React from "react";
-import data from '../../data/MOCK_DATA.json'
-import TrieNode from '../../processing/trie.js'
+import React, { useEffect } from "react";
+import CitizenData from "../../data/MOCK_DATA.json";
+import { insert, search } from "../../processing/trie.js";
+import _ from "lodash";
+import { useState } from "react";
 
-function CitizenSearch({ posts }) {
+const CitizenSearch = ({ tree }) => {
+    const [suggestion, setSuggestion] = useState([]);
+    const onSearchChange = (e) => {
+        if (e.target.value == "") setSuggestion([]);
+        else {
+            const ans = search(tree, e.target.value);
+            // const ans = []
+            // CitizenData.every((info) => {
+            //     if (info.name.startsWith(e.target.value))
+            //         ans.push(info.name)
+            //     if (ans.length > 10)
+            //         return false
+            //     else
+            //         return true
+            // })
+            setSuggestion(ans);
+        }
+    };
+
     return (
-        <div className={"w-screen h-screen bg-slate-300 flex justify-center"}>
+        <div
+            className={[
+                `w-screen min-h-screen bg-slate-300 flex justify-center`,
+            ]}
+        >
             <div className="w-3/5 my-2">
-                <form>
-                    <div>
+                <form className="flex w-full">
+                    <div className="w-full">
                         <input
                             type="text"
                             id="name"
-                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm ${
+                                suggestion.length > 0
+                                    ? "rounded-t-lg"
+                                    : "rounded-lg"
+                            } focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
                             placeholder="Search here"
+                            onChange={(e) => onSearchChange(e)}
                         />
+                        <div className="">
+                            {suggestion.map((item) => {
+                                return (
+                                    <h1
+                                        className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
+                                    >
+                                        {item}
+                                    </h1>
+                                );
+                            })}
+                        </div>
                     </div>
                     <button
                         type="submit"
-                        class="float-right my-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        class="h-fit mx-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        onClick={(e) => e.preventDefault()}
                     >
                         Submit
                     </button>
@@ -25,20 +66,18 @@ function CitizenSearch({ posts }) {
             </div>
         </div>
     );
-}
+};
 
 export async function getStaticProps() {
-    const root = TrieNode()
-    data.forEach((infomation) => {
-        infomation.name.forEach((letter) => {
-            root.insert()
-        })
-    })
-    return {
-      props: {
-        res,
-      },
+    let tree = {
+        endOfWord: false,
+        letter: null,
+        children: {},
+    };
+    for (const data of CitizenData) {
+        tree = insert(tree, data.name, data);
     }
-  }
+    return { props: { tree } };
+}
 
 export default CitizenSearch;
--- processing/trie.js
+++ processing/trie.js
@@ -1,10 +1,46 @@
-export default class TrieNode {
-    constructor(letter = null, endOfWord = false, content = {}) {
-        this.letter = letter
-        this.endOfWord = endOfWord
-        this.children = {}
-        this.content = content
+import _ from "lodash";
+
+export const insert = (root = {}, word = "", content = {}) => {
+    const letterList = word.split("");
+    let node = _.clone(root);
+    for (const lt of letterList) {
+        if (!node.children[lt]) {
+            const newNode = {
+                endOfWord: false,
+                letter: lt,
+                children: {},
+            };
+            node.children[lt] = newNode;
+            node = _.clone(newNode);
+        } else node = _.clone(node.children[lt]);
     }
+    node.endOfWord = true;
+    node.content = content;
+    return root;
+};
 
+export const search = (root = {}, str = "") => {
+    const res = [];
+    const DFS = (root, _str) => {
+        if (Object.keys(root.children).length > 0)
+            Object.keys(root.children).forEach((item) => {
+                _str += item;
+                return DFS(root.children[item], _str);
+            });
+        else if (_str) {
+            res.push(_str);
+        }
+    };
 
-}
+    let node = _.clone(root);
+    str.split("").forEach((letter) => {
+        if (!node.children[letter]) return [];
+        else {
+            node = node.children[letter];
+        }
+    });
+    Object.keys(node.children).forEach((item) => {
+        DFS(node.children[item], str + item);
+    });
+    return res;
+};
